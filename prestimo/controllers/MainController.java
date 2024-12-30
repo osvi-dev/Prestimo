ca3f5e0990ea34c499e8648a473a508c0687ece0
osvi-dev <joseconstantino987@gmail.com>
2024-12-30 13:16:05 -0600

    Feature: conect with db, add users

--- prestimo/controllers/MainController.java
+++ prestimo/controllers/MainController.java
@@ -1,8 +1,11 @@
 package prestimo.controllers;
 
+import prestimo.models.local.Usuarios;
+
 public class MainController {
-    
-    
+    public static void main(String[] args) {
+        Usuarios usuarios = new Usuarios();
+        usuarios.insertarUsuario("Osvaldo", "Constantino", "Bautista", "osvi2", "hola3@hola.com");
 
-
+    }
 }
---
+++ prestimo/models/local/Roles.java
@@ -0,0 +1,10 @@
+package prestimo.models.local;
+
+public class Roles {
+
+    private String rol;
+     
+    
+    
+
+}
