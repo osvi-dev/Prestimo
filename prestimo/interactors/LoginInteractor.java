d08fd53f8932c155c36b9da5809f5a4902bbd200
osvi-dev <joseconstantino987@gmail.com>
2025-01-05 20:08:46 -0600

    Add views and javadoc in login interactor

--- prestimo/interactors/LoginInteractor.java
+++ prestimo/interactors/LoginInteractor.java
@@ -30,13 +30,30 @@ public class LoginInteractor {
             System.out.println(usuario.veficarCredenciales(usernameOrEmail, "", password));
         }   
     }
-    
+
+    /**
+     * Verifica si una cadena de texto corresponde a un formato de correo electrónico válido.
+     *
+     * @param input La cadena de texto a verificar.
+     * @return true si la cadena cumple con el formato de correo electrónico, false en caso contrario.
+     */
     public static boolean veficarCorreo(String input) {
         String emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
         return Pattern.matches(emailRegex, input);
     }
 
-    // Método para validar nombres de usuario
+    /**
+     * Verifica si una cadena de texto corresponde a un formato de nombre de usuario válido.
+     *
+     * El nombre de usuario debe cumplir con las siguientes reglas:
+     * 
+     * Debe contener entre 3 y 20 caracteres.
+     *Solo puede contener letras, números, puntos (.), guiones bajos (_) y guiones (-).
+     * 
+     *
+     * @param input La cadena de texto a verificar.
+     * @return true si la cadena cumple con el formato de nombre de usuario, false en caso contrario.
+     */
     public static boolean verificarUsername(String input) {
         String usernameRegex = "^[a-zA-Z0-9._-]{3,20}$";
         return Pattern.matches(usernameRegex, input);
